f2aaf1b4289c4d7ef8f0b201435f238c53fca28d
singh-aparna <singh123aparna@gmail.com>
2025-02-03 00:40:22 +0530

    Added more example on Destructuring concept

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+//CommonJS (require/module.exports) is used in Node.js for synchronous module loading
 const app = express();
 app.get('/', (req, res) => {
     res.send("It's working!");
 })
+//res.json() automatically sets the Content-Type to application/json and converts the response to JSON,
+//while res.send() can send any type of response (string, object, buffer, etc.).
 app.listen(4000, () => {
     console.log("Server is listenning on PORT 4000!");
 })
