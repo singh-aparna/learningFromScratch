34b592ff62c041a51e2643e7d3d75c8193315957
singh-aparna <singh123aparna@gmail.com>
2025-02-02 23:29:50 +0530

    Added more example on Destructuring concept

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("h1")//Fetches the DOM element with the ID h1.
-    .textContent = "Hi, Learn from scratch!";//Sets the text content of that element to "Hi".
+    .textContent = "Hi, World!";//Sets the text content of that element to "Hi".
 
 
-document.getElementById("h1")//Fetches the DOM element with the ID h1.
+document.getElementById("h2")//Fetches the DOM element with the ID h1.
     .innerHTML = "<h1>Hi, Learn from scratch!!</h1>";//Sets the text content+h1 element of that element to "Hi".
