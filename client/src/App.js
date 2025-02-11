7f5dcab9f43bfa83e8c6146c15dd9d519bea8e32
singh-aparna <singh123aparna@gmail.com>
2025-02-11 11:11:40 +0530

    Initialize a new rect directory

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useEffect, useState } from "react";
+
+function App() {
+  const [input, setInput] = useState('');
+  return (
+    <main>
+      
+    </main>
+  );
+}
+
+export default App;
